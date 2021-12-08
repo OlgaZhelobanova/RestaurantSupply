9501869745788477746e3084efd4a757f79d9f44
OlgaZhelobanova <olius2804@gmail.com>
2021-12-08 18:13:48 +0300

    Create .suo

---
+++ Kursovaya/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using BLLnew;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Kursovaya
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        CRUD db = new CRUD();
+        List<ProductBLL> allProducts;
+        List<OrderBLL> allOrders;
+        public MainWindow()
+        {
+            InitializeComponent();
+            loadData();
+            
+         }
+
+        void loadData()
+        {
+            loadProducts();
+            loadOrders();
+        }
+
+        void loadProducts()
+        {
+            allProducts = db.GetAllProducts();
+            ProductsDataGrid.ItemsSource = allProducts;
+        }
+
+        void loadOrders()
+        {
+            allOrders = db.GetAllOrders();
+            OrdersDataGrid.ItemsSource = OrderDop.Orderdop();
+        }
+
+    }
+}
